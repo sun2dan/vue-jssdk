bcc974aa71770bcb688659d52aabc8743db498b4
sunchao21 <sunchao5@jd.com>
2021-11-19 10:08:24 +0800

    refactor: 优化css前缀，添加sass-loader

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div :class="sdkId"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  props: {},
+  data() {
+    return {
+      sdkId: "",
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {
+    this.sdkId = process.env.VUE_APP_SDKID;
+  },
+  mounted() {},
+  methods: {},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./assets/style/main";
 </style>
